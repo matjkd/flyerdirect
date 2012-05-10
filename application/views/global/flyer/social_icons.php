5bad76b39528d555c975bf58650f1d5142346809
Mat Sadler <mat@redstudio.co.uk>
2012-05-10 11:44:18 +0000

    added rss feed link to feedburner

--- application/views/global/flyer/social_icons.php
+++ application/views/global/flyer/social_icons.php
@@ -4,4 +4,6 @@
 
 <a href="https://twitter.com/#!/flyerdirect"/><img src="<?=base_url()?>images/icons/twitter_32.png" /></a>
 <a href="http://uk.linkedin.com/in/leafletdistribution"/><img src="<?=base_url()?>images/icons/linkedin_32.png" /></a>
+<a href="http://feeds.feedburner.com/Flyerdirect" rel="alternate" type="application/rss+xml">
+<img src="http://www.feedburner.com/fb/images/pub/feed-icon32x32.png" alt="" style="width:32px; height:32px; border:0"/></a>&nbsp;<a href="http://feeds.feedburner.com/Flyerdirect" rel="alternate" type="application/rss+xml"></a>
 </div>
